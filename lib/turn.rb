56fb39742ccf34d16e79c28d32a92b25767863cb
Emilia Ziemichód <emifrompoland@gmail.com>
2019-01-02 19:34:12 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -30,8 +30,8 @@ end
 def turn(board)
   puts "Please enter 1-9:"
   input=gets.strip
-  input_to_index(input)
-   #if valid_move?(board,input)
+  index=input_to_index(input)
+   #if valid_move?(board,index)
    #  move(board,index)
    #  display_board(board)
    #else
